f60bd5f2a8693fb8e1183104c2f828357a17f0ac
Joseph John <joseph.john@anu.edu.au>
2024-11-10 10:58:29 +1100

    loop

---
+++ day1/if_else.m
@@ -0,0 +1,6 @@
+x = 5;
+if x > 10
+    disp('x is greater than 10');
+else
+    disp('x is 10 or less');
+end
---
+++ day1/if_else.py
@@ -0,0 +1,20 @@
+x = 5
+
+if x > 10:
+    print("x is greater than 10")
+elif x == 10:
+    print("x is equal to 10")
+else:
+    print("x is less than 10")
+
+'''
+Indentation in Python is crucial because it defines the structure and flow of the code. 
+Unlike many other languages that use braces or keywords to mark code blocks, Python uses 
+indentation to group statements. Each level of indentation indicates a new block or scope, 
+which makes it both a syntax requirement and an organizational tool. 
+
+This strict indentation enforces readability and uniform coding style, helping developers 
+quickly understand code logic. Errors in indentation lead to syntax errors.
+
+'''
+
